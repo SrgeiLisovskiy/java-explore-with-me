d2aa934c69b8ced68bbbc5915f1f55187616ec9a
Sergey <19lis91@gmail.com>
2023-10-26 14:44:52 +0300

    первая проверка

---
+++ stats-service/stats-server/src/main/java/ru/practicum/server/model/Stat.java
@@ -0,0 +1,31 @@
+package ru.practicum.server.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "stat")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Stat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "app", nullable = false)
+    private String app;
+    @Column(name = "uri", nullable = false)
+    private String uri;
+    @Column(name = "ip", nullable = false)
+    private String ip;
+    @Column(name = "time_stamp", nullable = false)
+    private LocalDateTime timestamp;
+
+}
---
+++ stats-service/stats-server/src/main/java/ru/practicum/server/utilite/Constant.java
@@ -0,0 +1,8 @@
+package ru.practicum.server.utilite;
+
+
+public class Constant {
+
+    public static final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+}
+
