76b5706473b0c7e887dbb892d6a44958a268ddfa
Sergey <19lis91@gmail.com>
2023-11-12 01:40:58 +0300

    первая проверка

--- main-service/src/main/java/ru/practicum/main/controller/priv/PrivateCommentController.java
+++ main-service/src/main/java/ru/practicum/main/controller/priv/PrivateCommentController.java
@@ -18,33 +18,37 @@ import java.util.List;
 @RequestMapping(path = "/users/{userId}/comments")
 public class PrivateCommentController {
     private final CommentService commentService;
+
     @PostMapping("/events/{eventId}")
     public ResponseEntity<CommentDto> createComment(@PathVariable Long userId,
                                                     @PathVariable Long eventId,
-                                                    @Valid @RequestBody NewCommentDto newCommentDto){
+                                                    @Valid @RequestBody NewCommentDto newCommentDto) {
         log.info("Выполнен запрос POST/users/{userId}/comments/events/{eventId}");
-        return new ResponseEntity<>(commentService.createComment(userId,eventId, newCommentDto), HttpStatus.CREATED);
+        return new ResponseEntity<>(commentService.createComment(userId, eventId, newCommentDto), HttpStatus.CREATED);
     }
+
     @GetMapping
-    public ResponseEntity<List<CommentDto>> getCommentsByUser(@PathVariable Long userId){
+    public ResponseEntity<List<CommentDto>> getCommentsByUser(@PathVariable Long userId) {
         log.info("Выполнен запрос GET/users/{userId}/comments");
-        return new ResponseEntity<>(commentService.getCommentsByUser(userId),HttpStatus.OK);
+        return new ResponseEntity<>(commentService.getCommentsByUser(userId), HttpStatus.OK);
     }
+
     @GetMapping("/{commentId}")
-    public ResponseEntity<CommentDto> getCommentsById(@PathVariable Long userId, @PathVariable Long commentId){
+    public ResponseEntity<CommentDto> getCommentsById(@PathVariable Long userId, @PathVariable Long commentId) {
         log.info("Выполнен запрос GET/users/{userId}/comments/{commentId}");
         return new ResponseEntity<>(commentService.getCommentsById(userId, commentId), HttpStatus.OK);
     }
+
     @DeleteMapping("/{commentId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteComment(@PathVariable Long userId, @PathVariable Long commentId){
+    public void deleteComment(@PathVariable Long userId, @PathVariable Long commentId) {
         log.info("Выполнен запрос DEL//users/{userId}/comments/{commentId}");
         commentService.deleteComment(userId, commentId);
     }
 
     @PatchMapping("/{commentId}")
     public ResponseEntity<CommentDto> updateComment(@PathVariable Long userId, @PathVariable Long commentId,
-                                                    @Valid @RequestBody NewCommentDto newCommentDto){
+                                                    @Valid @RequestBody NewCommentDto newCommentDto) {
         log.info("Выполнен запрос PATCH//users/{userId}/comments/{commentId}");
         return new ResponseEntity<>(commentService.updateComment(userId, commentId, newCommentDto), HttpStatus.OK);
 
--- main-service/src/main/java/ru/practicum/main/repository/CommentRepository.java
+++ main-service/src/main/java/ru/practicum/main/repository/CommentRepository.java
@@ -10,8 +10,11 @@ import java.util.List;
 import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long>, JpaSpecificationExecutor<Comment> {
-    Optional<Comment> findByAuthor_IdAndId(Long userId, Long commentId );
+    Optional<Comment> findByAuthor_IdAndId(Long userId, Long commentId);
+
     List<Comment> findByAuthor_Id(Long userId);
+
     Page<Comment> findByEvent_Id(Long eventId, Pageable pageable);
+
     Long countCommentByEvent_Id(Long eventId);
 }
